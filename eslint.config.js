24df37a615cfa6e4fa83436e7be5d584e8f3ec7b
Becmi <emibecerra92@gmail.com>
2026-01-03 14:19:54 -0300

    edit config and package

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from "@vitejs/plugin-react"
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    plugins: [react()],
+    base: "/GHAutomotores/",
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
